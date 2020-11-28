9640a960b756b2fb3812461d6d46c66d3caf6ef3
Kyle Coberly <kyle.coberly@gmail.com>
2020-11-27 22:11:10 -0700

    Fixes indentation

--- app/src/App.vue
+++ app/src/App.vue
@@ -18,15 +18,15 @@
     <div class="content">
       <h2><img alt="Rex Goliath" src="/rex-goliath-logo.png" /></h2>
       <h3>His Royal Majesty is back.</h3>
-    <p>America's most unhinged rooster is out of retirement, and this time? He's mad as hell.
+      <p>America's most unhinged rooster is out of retirement, and this time? He's mad as hell.
     Experience the unapologetic flavor burst of Rex Goliath.</p>
     <Controls />
-  </div>
-</header>
-<main>
-  <Category :wines="drinkAgain" heading="Drink Again" />
-  <Category :wines="keepDrinking" heading="Keep Drinking" />
-</main>
+    </div>
+  </header>
+  <main>
+    <Category :wines="drinkAgain" heading="Drink Again" />
+    <Category :wines="keepDrinking" heading="Keep Drinking" />
+  </main>
 </template>
 
 <script>
