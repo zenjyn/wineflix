935c5c6e8d8f4c3faac4ec8bb62e68a9a7b8f861
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-13 12:10:52 -0700

    Refactored to static

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,144 +1,183 @@
 <template>
-  <Hero :heroContent="heroContent" />
+  <header class="hero">
+    <img class="hero-image" src="/wine-hero.webp">
+    <nav class="navigation">
+      <section class="primary-navigation">
+        <h1>WineFlix</h1>
+        <ul class="navigation-links">
+          <li class="active">
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Whites</a>
+          </li>
+          <li><a href="#">Reds</a>
+          </li>
+          <li><a href="#">Blends</a></li>
+        </ul>
+      </section>
+      <section class="secondary-navigation">
+        <span class="user-avatar">
+          <a href="#">S</a>
+        </span>
+      </section>
+    </nav>
+    <div class="hero-content">
+      <h2>
+        <img alt="Rex Goliath" src="/rex-goliath-logo.png">
+      </h2>
+      <h3>His Royal Majesty is back.</h3>
+      <p>
+        America's most unhinged rooster is out of retirement, and this time? He's mad
+        as hell.  Experience the unapologetic flavor burst of Rex Goliath.
+      </p>
+      <ul class="controls">
+        <li>
+          <button class="primary-action">
+            <FontAwesomeIcon icon="play" />
+            <span>Drink</span>
+          </button>
+        </li>
+        <li>
+          <button class="secondary-action">
+            <FontAwesomeIcon icon="info-circle" />
+            <span>More Info</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </header>
   <main>
-    <Category :wines="drinkAgain" heading="Drink Again" />
-    <Category :wines="keepDrinking" heading="Keep Drinking" />
+    <section class="category">
+      <h2>Drink Again</h2>
+      <ul>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <span class="new-badge">New!</span>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <span class="new-badge">New!</span>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <span class="new-badge">New!</span>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section class="category">
+      <h2>Keep Drinking</h2>
+      <ul>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="wine">
+            <img src="/red-wine.jpg" alt="Wine">
+            <div class="amount-consumed">
+              <progress min="0" max="100" value="22"></progress>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
   </main>
 </template>
 
 <script>
-import Category from './components/Category.vue';
-import Hero from './components/Hero.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
   name: 'App',
   components: {
-    Category,
-    Hero,
-  },
-  computed: {
-    heroContent() {
-      return {
-        title: 'Rex Goliath',
-        subtitle: 'His Royal Majesty is back.',
-        imageUrl: '/rex-goliath-logo.png',
-        description: `America's most unhinged rooster is out of retirement, and this time? He's mad
-        as hell.  Experience the unapologetic flavor burst of Rex Goliath.`,
-      };
-    },
-    wines() {
-      return [{
-        id: 1,
-        imageUrl: '/red-wine.jpg',
-        isNew: true,
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 2,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 3,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isNew: true,
-        isFinished: true,
-      }, {
-        id: 4,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 5,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isNew: true,
-        isFinished: true,
-      }, {
-        id: 6,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 7,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 8,
-        imageUrl: '/red-wine.jpg',
-        label: 'Wine',
-        isFinished: true,
-      }, {
-        id: 1,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 2,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 3,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 4,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 5,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 6,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 7,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }, {
-        id: 8,
-        imageUrl: '/red-wine.jpg',
-        progress: 22,
-        label: 'Wine',
-      }];
-    },
-    drinkAgain() {
-      return this.wines.filter((wine) => wine.isFinished);
-    },
-    keepDrinking() {
-      return this.wines.filter((wine) => wine.progress);
-    },
+    FontAwesomeIcon,
   },
 };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fira+Sans:wght@400;700&display=swap");
-
-img {
-  max-width: 100%;
-}
-a {
-  color: inherit;
-  text-decoration: none;
-}
-body {
-  font-family: "Fira Sans", sans-serif;
-  background-color: hsl(0, 0%, 10%);
-}
-
-main {
-  padding: 18px 48px;
-  margin-top: -72px;
-}
 </style>
